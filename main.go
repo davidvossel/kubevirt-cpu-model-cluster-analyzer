07750f928b102aec0c38aab6f4c2bbee50603d2b
David Vossel <davidvossel@gmail.com>
2024-09-11 15:29:56 -0400

    parse cpu and host models by node and aggregate results
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"sigs.k8s.io/yaml"
 )
+
+const (
+	cpuModelLabel  = "cpu-model.node.kubevirt.io/"
+	hostModelLabel = "host-model-cpu.node.kubevirt.io/"
+)
+
+type modelStats struct {
+	HostModelNodeCount  int `json:"isHostModel"`
+	CompatibleNodeCount int `json:"compatibleNodeCount"`
+	TotalNodeCount      int `json:"totalNodeCount"`
+}
+type results struct {
+	CPUModels map[string]modelStats
+}
 
 type metadata struct {
 	Name   string            `json:"name"`
@@ -22,8 +37,59 @@ type nodeList struct {
 	Items []node `json:"items"`
 }
 
-func main() {
+func parseModelLabel(key string) (string, bool) {
+	isHostModel := false
+	if strings.Contains(key, cpuModelLabel) {
+		//fall through
+	} else if strings.Contains(key, hostModelLabel) {
+		isHostModel = true
+	} else {
+		// not a cpu model related label
+		return "", false
+	}
 
+	str := strings.Split(key, "/")
+	if len(str) != 2 {
+		panic(fmt.Errorf("invalid cpu model label encountered [%s]", key))
+	}
+	return str[1], isHostModel
+}
+
+func findCommonSupportedModels(nodes []node) results {
+	res := results{
+		CPUModels: make(map[string]modelStats),
+	}
+
+	for _, node := range nodes {
+		// TODO ignore nodes that are not virt capable
+		for key, val := range node.ObjMeta.Labels {
+			if val != "true" {
+				continue
+			}
+			model, isHostModel := parseModelLabel(key)
+			if model == "" {
+				continue
+			}
+			stats, exists := res.CPUModels[model]
+			if !exists {
+				stats = modelStats{
+					TotalNodeCount: len(nodes),
+				}
+			}
+
+			stats.CompatibleNodeCount++
+			if isHostModel {
+				stats.HostModelNodeCount++
+			}
+			res.CPUModels[model] = stats
+
+		}
+	}
+
+	return res
+}
+
+func main() {
 	bytes, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		panic(fmt.Errorf("Failed to read stdin: %v", err))
@@ -35,9 +101,12 @@ func main() {
 		panic(fmt.Errorf("Error unmarshalling YAML nodelist: %v", err))
 	}
 
-	fmt.Printf("node list length: %d\n", len(nodeList.Items))
+	res := findCommonSupportedModels(nodeList.Items)
 
-	for _, node := range nodeList.Items {
-		fmt.Printf("node: %v\n", node)
+	// TODO source CPUModel results by rank
+
+	for model, stats := range res.CPUModels {
+		fmt.Printf("CPU Model [%s] compatible with [%d] out of [%d] nodes\n", model, stats.CompatibleNodeCount, stats.TotalNodeCount)
+		fmt.Printf("CPU Model [%s] hostModel with [%d] out of [%d] nodes\n", model, stats.HostModelNodeCount, stats.TotalNodeCount)
 	}
 }
